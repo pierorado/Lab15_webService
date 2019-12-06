bbc98472630ba8fa296388623ca361330122e45e
delpiero ramos ramos <delpiee@hotmail.com>
2019-12-06 12:23:26 -0500

    Agregar archivos de proyecto.

---
+++ Lab15_Xamarin/views/AddProduct.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Net.Http;
+
+using Lab15_Xamarin.models;
+using Lab15_Xamarin.views;
+
+using Newtonsoft.Json;
+
+namespace Lab15_Xamarin.views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddProduct : ContentPage
+    {
+        public AddProduct()
+        {
+            InitializeComponent();
+        }
+
+      
+    }
+}
---
+++ Lab15_Xamarin/views/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Lab15_Xamarin.models;
+using Lab15_Xamarin.views;
+using Newtonsoft.Json;
+
+namespace Lab15_Xamarin
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            GetPersonal();
+        }
+
+        private async void GetPersonal()
+        {
+            HttpClient client = new HttpClient();
+            string url = "https://agenda21.000webhostapp.com/lista.php";
+            var response = await client.GetStringAsync(url);
+            var personal = JsonConvert.DeserializeObject<List<ProductModel>>(response);
+            listOfPersonal.ItemsSource = personal;
+
+        }
+    }
+}
